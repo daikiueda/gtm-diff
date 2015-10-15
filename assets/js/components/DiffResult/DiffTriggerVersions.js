5d4a115b586fdab153cfd3388d4f2cc9c4ec186e
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2015-10-15 11:43:52 +0900

    差分表示を種別ごとに最適化するための処理フローを追加した。

--- assets/js/components/DiffResult/DiffTriggerVersions.js
+++ assets/js/components/DiffResult/DiffTriggerVersions.js
@@ -1,4 +1,26 @@
 import React from 'react';
 import DiffVersionsCommon from './DiffVersionsCommon.js';
 
-export default class DiffTagVersions extends DiffVersionsCommon{};
+export default class DiffTagVersions extends DiffVersionsCommon{
+
+    createParticularRows( params ){
+        switch( params[ 0 ] ){
+            case 'filter':
+                return this.createFilterRows( params );
+            default:
+                return super.createParticularRows( params );
+        }
+    }
+
+    createFilterRows( params ){
+        return null;
+
+        return (
+            <tr>
+                <th>hoge</th>
+                <td>hoge</td>
+                <td>hoge</td>
+            </tr>
+        );
+    }
+};
--- assets/js/components/DiffResult/DiffVersionsCommon.js
+++ assets/js/components/DiffResult/DiffVersionsCommon.js
@@ -3,24 +3,17 @@ import isEqual from 'lodash.isequal';
 
 export default class DiffVersionsCommon extends Component {
 
-    createElementTitle( content ){
-        var nameA = content[ 1 ] && content[ 1 ].name,
-            nameB = content[ 2 ] && content[ 2 ].name;
-
-        if( nameA && nameB ){
-            return nameA === nameB ?
-                <h3>{nameA}</h3>:
-                <h3>{nameA} | {nameB}</h3>;
-        }
-        else {
-            return <h3>{nameA || nameB}</h3>
+    createParticularRows( params ){
+        switch( params[ 0 ] ){
+            case 'parameter':
+                return this.createParameterRows( params );
+            default:
+                return null;
         }
     }
 
-    createDataBodySubRows( params ){
-        var primaryPrams,
-            secondaryParams,
-            rows = [ <tr className="complex"><th colSpan="3">{params[ 0 ]}</th></tr> ],
+    createParameterRows( params ){
+        var rows = [ <tr className="complex"><th colSpan="3">{params[ 0 ]}</th></tr> ],
             addedKeys = {};
 
         function addRow( key, valueA, valueB ){
@@ -67,47 +60,45 @@ export default class DiffVersionsCommon extends Component {
     }
 
     createDataBody( content ){
-        var complexData = [],
-            dataBody = Object.keys( content[ 1 ] || content[ 2 ] ).map( key => {
-                if( key.match( DiffVersionsCommon.IGNORE_KEYS ) ){
-                    return null;
-                }
+        // ToDo: 一方にしかないキーがある場合、表示漏れの可能性
+        return Object.keys( content[ 1 ] || content[ 2 ] ).map( key => {
 
-                var valueA = content[ 1 ] && DiffVersionsCommon.flattenSimpleObject( content[ 1 ][ key ] ),
-                    valueB = content[ 2 ] && DiffVersionsCommon.flattenSimpleObject( content[ 2 ][ key ] );
+            if( key.match( DiffVersionsCommon.IGNORE_KEYS ) ){
+                return null;
+            }
 
-                if( ( valueA || valueB ) instanceof Array && typeof ( valueA || valueB )[ 0 ] === 'object' && ( valueA || valueB )[ 0 ].hasOwnProperty( 'key' ) ){
-                    return this.createDataBodySubRows( [ key, valueA, valueB ] );
-                }
-                switch( typeof ( valueA || valueB ) ){
-                    // ToDo: typeof ( false || null ) も、「object」となるので要注意。
-                    case 'object':
-                    case 'string':
-                    case 'boolean':
-                        return (
-                            <tr className={ isEqual( valueA, valueB ) ? 'notModified': 'modified' }>
-                                <th>{key}</th>
-                                <td>{DiffVersionsCommon.formatValue( valueA )}</td>
-                                <td>{DiffVersionsCommon.formatValue( valueB )}</td>
-                            </tr>
-                        );
-                    default:
-                        console.warn( key, typeof ( valueA || valueB ), valueA, valueB );
-                }
-            } );
+            var valueA = content[ 1 ] && DiffVersionsCommon.flattenSimpleObject( content[ 1 ][ key ] ),
+                valueB = content[ 2 ] && DiffVersionsCommon.flattenSimpleObject( content[ 2 ][ key ] ),
 
-        return (
-            <tbody>
-                {dataBody}
-            </tbody>
-        );
+                particularData = this.createParticularRows( [ key, valueA, valueB ] );
+
+            if( particularData ){
+                return particularData;
+            }
+            switch( typeof ( valueA || valueB ) ){
+                // ToDo: typeof ( false || null ) も、「object」となるので要注意。
+                case 'object':
+                case 'string':
+                case 'number':
+                case 'boolean':
+                    return (
+                        <tr className={ isEqual( valueA, valueB ) ? 'notModified': 'modified' }>
+                            <th>{key}</th>
+                            <td>{DiffVersionsCommon.formatValue( valueA )}</td>
+                            <td>{DiffVersionsCommon.formatValue( valueB )}</td>
+                        </tr>
+                    );
+                default:
+                    console.warn( key, typeof ( valueA || valueB ), valueA, valueB );
+            }
+        } );
     }
 
     render(){
         var sections = this.props.contents.map( ( content ) => {
             return (
                 <section className={content[ 0 ]? 'modified': 'notModified'}>
-                    {this.createElementTitle( content )}
+                    {DiffVersionsCommon.createElementTitle( content )}
                     <table>
                         {this.createDataBody( content )}
                     </table>
@@ -119,30 +110,39 @@ export default class DiffVersionsCommon extends Component {
     }
 }
 
-Object.assign(
-    DiffVersionsCommon,
-    {
-        IGNORE_KEYS: /^(accountId|containerId|tagId|triggerId|variableId|fingerprint)$/,
+DiffVersionsCommon.IGNORE_KEYS = /^(accountId|containerId|tagId|triggerId|variableId|fingerprint)$/;
 
-        formatValue: function( value ){
-            switch( typeof value ){
-                case 'object':
-                    return ( value === null )? '': JSON.stringify( value );
-                case 'boolean':
-                    return value ? 'true': 'false';
-                default:
-                    return value;
-            }
-        },
-
-        flattenSimpleObject: function( value ){
-            if( !value || typeof value != 'object' || value instanceof Array ){
-                return value;
-            }
-            if( Object.keys( value ).length === 2 && value.hasOwnProperty( 'type' ) && value.hasOwnProperty( 'value' ) ){
-                return value.value;
-            }
+DiffVersionsCommon.formatValue = function( value ){
+    switch( typeof value ){
+        case 'object':
+            return ( value === null )? '': JSON.stringify( value );
+        case 'boolean':
+            return value ? 'true': 'false';
+        default:
             return value;
-        }
     }
-);
+};
+
+DiffVersionsCommon.flattenSimpleObject = function( value ){
+    if( !value || typeof value != 'object' || value instanceof Array ){
+        return value;
+    }
+    if( Object.keys( value ).length === 2 && value.hasOwnProperty( 'type' ) && value.hasOwnProperty( 'value' ) ){
+        return value.value;
+    }
+    return value;
+};
+
+DiffVersionsCommon.createElementTitle = function( content ){
+    var nameA = content[ 1 ] && content[ 1 ].name,
+        nameB = content[ 2 ] && content[ 2 ].name;
+
+    if( nameA && nameB ){
+        return nameA === nameB ?
+            <h3>{nameA}</h3>:
+            <h3>{nameA} | {nameB}</h3>;
+    }
+    else {
+        return <h3>{nameA || nameB}</h3>
+    }
+};
