10112d459d76bb43042554ee956d9bf68eb127c3
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2015-10-17 16:18:04 +0900

    react-mdl, postsを導入。

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -2,7 +2,9 @@ const SETTINGS = {
     JS_LIBS: {
         SRC: [
             './bower_components/react/react.js',
-            './bower_components/react/react-dom.js'
+            './bower_components/react/react-dom.js',
+
+            './node_modules/react-mdl/extra/material.js'
         ],
         DEST: '../public/js',
         FILENAME: 'libs.js'
@@ -11,6 +13,12 @@ const SETTINGS = {
         SRC: './js/app.js',
         DEST: '../public/js',
         FILENAME: 'app.js'
+    },
+
+    CSS: {
+        SRC: './css',
+        DEST: '../public/css',
+        FILENAME: 'app.css'
     }
 };
 
@@ -23,15 +31,18 @@ var del = require( 'del' ),
     gutil = require( 'gulp-util' ),
     runSequence = require('run-sequence' ),
     plumber = require( 'gulp-plumber' ),
+
     concat = require( 'gulp-concat' ),
     uglify = require( 'gulp-uglify' ),
-    ghPages = require( 'gulp-gh-pages' ),
-
     sourcemaps = require( 'gulp-sourcemaps' ),
     browserify = require( 'browserify' ),
     watchify = require( 'watchify' ),
 
-    browserSync = require( 'browser-sync' ).create();
+    postcss = require( 'gulp-postcss' ),
+    postcssImport = require( 'postcss-import' ),
+
+    browserSync = require( 'browser-sync' ).create(),
+    ghPages = require( 'gulp-gh-pages' );
 
 
 var b = browserify( Object.assign( {}, watchify.args, {
@@ -64,39 +75,56 @@ gulp.task( 'build:js:watch', function(){
     b = watchify( b );
     b.on( 'update', bundleJS );
     b.on( 'log', gutil.log );
-    bundleJS();
+    return bundleJS();
 } );
 
 gulp.task( 'build:jsLibs', function(){
-    gulp.src( SETTINGS.JS_LIBS.SRC )
+    return gulp
+        .src( SETTINGS.JS_LIBS.SRC )
         .pipe( uglify() )
         .pipe( concat( SETTINGS.JS_LIBS.FILENAME ) )
         .pipe( gulp.dest( SETTINGS.JS_LIBS.DEST ) );
 } );
 
+
+gulp.task( 'build:css', function(){
+    return gulp
+        .src( [ SETTINGS.CSS.SRC, SETTINGS.CSS.FILENAME ].join( '/' ) )
+        .pipe( postcss( [
+            postcssImport
+        ] ) )
+        .pipe( gulp.dest( SETTINGS.CSS.DEST ) );
+} );
+
+gulp.task( 'build:css:watch', function(){
+    return gulp.watch( [ SETTINGS.CSS.SRC, '**/*.*' ].join( '/' ), [ 'build:css' ] );
+} );
+
+
 gulp.task( 'build', function( callback ){
     // ToDo: clean
-    runSequence( [ 'build:jsLibs', 'build:js' ], callback  );
+    runSequence( [ 'build:css', 'build:jsLibs', 'build:js' ], callback  );
 } );
 
 
 gulp.task( 'browserSync', function(){
     browserSync.init( {
+        files: [ '../public/**/*.*', '../public/*.*' ],
         server: {
             baseDir: '../public/',
             https: true
         }
     } );
-
-    return gulp.watch( '../public/**/*.*' ).on( 'change', browserSync.reload );
+    browserSync.emitter.on( 'reload', gutil.log );
 } );
 
 gulp.task( 'server', function( callback ){
-    runSequence( 'build', 'build:js:watch', 'browserSync', callback  );
+    runSequence( 'build', [ 'build:css:watch', 'build:js:watch' ], 'browserSync', callback  );
 } );
 
 gulp.task( 'gh-pages', function(){
-    return gulp.src( '../public/**/*' )
+    return gulp
+        .src( '../public/**/*' )
         .pipe( ghPages() );
 } );
 
