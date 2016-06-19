8c475ed20f90c80fbd7721d335bde09cf5e5f5e3
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2016-06-19 20:14:13 +0900

    gulpタスクみなおし
    * Webpackも気になるけれど、ひとまず既存ベースで見直し
    * fix code-style

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,3 +1,5 @@
+/* es */
+
 const SETTINGS = {
     JS_LIBS: {
         SRC: [
@@ -20,112 +22,132 @@ const SETTINGS = {
 };
 
 
-var del = require( 'del' ),
-    source = require( 'vinyl-source-stream' ),
-    buffer = require( 'vinyl-buffer' ),
+var del = require('del'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
 
-    gulp = require( 'gulp' ),
-    gutil = require( 'gulp-util' ),
-    runSequence = require('run-sequence' ),
-    plumber = require( 'gulp-plumber' ),
-    concat = require( 'gulp-concat' ),
-    uglify = require( 'gulp-uglify' ),
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    runSequence = require('run-sequence'),
 
-    sourcemaps = require( 'gulp-sourcemaps' ),
-    browserify = require( 'browserify' ),
-    watchify = require( 'watchify' ),
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber'),
 
-    sass = require( 'gulp-sass' ),
-    postcss = require( 'gulp-postcss' ),
-    postcssImport = require( 'postcss-import' ),
-    autoprefixer = require( 'autoprefixer' ),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
 
-    browserSync = require( 'browser-sync' ).create(),
-    ghPages = require( 'gulp-gh-pages' );
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    postcssImport = require('postcss-import'),
+    autoprefixer = require('autoprefixer'),
+
+    browserSync = require('browser-sync').create(),
+    ghPages = require('gulp-gh-pages');
 
 
-var b = browserify( {
+var b = browserify({
     cache: {},
     packageCache: {},
     debug: true,
-    entries: [ SETTINGS.JS.SRC ]
-} );
+    entries: [SETTINGS.JS.SRC]
+});
 
-function bundleJS(){
+function bundleJS() {
     return b
-        .transform( 'babelify', { presets: [ "es2015", "react" ] } )
-        .transform( 'browserify-shim', { global: true } )
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .transform('browserify-shim', {global: true})
         .bundle()
-        .on( 'error', function( err ){
-            console.log( err.message );
-            browserSync.notify( err.message, 3000 );
-            this.emit( 'end' );
-        } )
-        .pipe( plumber() )
-        .pipe( source( SETTINGS.JS.FILENAME ) )
-        .pipe( buffer() )
-        .pipe( sourcemaps.init( { loadMaps: true } ) )
-        .pipe( uglify() )
-        .pipe( sourcemaps.write( './') )
-        .pipe( gulp.dest( SETTINGS.JS.DEST ) );
+        .on('error', function(err) {
+            console.log(err.message);
+            browserSync.notify(err.message, 3000);
+            this.emit('end');
+        })
+        .pipe(plumber())
+        .pipe(source(SETTINGS.JS.FILENAME))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(SETTINGS.JS.DEST));
 }
 
-gulp.task( 'build:js', bundleJS );
+gulp.task('build:js', bundleJS);
 
-gulp.task( 'build:js:watch', function(){
-    b.plugin( watchify );
-    b.on( 'update', bundleJS );
-    b.on( 'log', gutil.log );
+gulp.task('build:js:watch', function() {
+    b.plugin(watchify);
+    b.on('update', bundleJS);
+    b.on('log', gutil.log);
     return bundleJS();
-} );
+});
 
-gulp.task( 'build:jsLibs', function(){
-    gulp.src( SETTINGS.JS_LIBS.SRC )
-        .pipe( uglify() )
-        .pipe( concat( SETTINGS.JS_LIBS.FILENAME ) )
-        .pipe( gulp.dest( SETTINGS.JS_LIBS.DEST ) );
-} );
+gulp.task('build:jsLibs', function() {
+    gulp.src(SETTINGS.JS_LIBS.SRC)
+        .pipe(uglify())
+        .pipe(concat(SETTINGS.JS_LIBS.FILENAME))
+        .pipe(gulp.dest(SETTINGS.JS_LIBS.DEST));
+});
+
+gulp.task('clean:js',
+    del.bind(null, [SETTINGS.JS.DEST, SETTINGS.JS_LIBS.DEST], {force: true})
+);
 
 
-gulp.task( 'build:css', function(){
+gulp.task('build:css', function() {
     return gulp
-        .src( SETTINGS.CSS.SRC )
-        .pipe( sourcemaps.init() )
-        .pipe( sass( { outputStyle: 'expanded' } ).on( 'error', sass.logError ) )
-        .pipe( postcss( [ postcssImport(), autoprefixer ] ) )
-        .pipe( sourcemaps.write( './' ) )
-        .pipe( gulp.dest( SETTINGS.CSS.DEST ) );
-} );
+        .src(SETTINGS.CSS.SRC)
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(postcss([postcssImport(), autoprefixer]))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(SETTINGS.CSS.DEST));
+});
 
-gulp.task( 'build:css:watch', function(){
-    return gulp.watch( SETTINGS.CSS.SRC, [ 'build:css' ] );
-} );
+gulp.task('build:css:watch', function() {
+    return gulp.watch(SETTINGS.CSS.SRC, ['build:css']);
+});
 
-
-gulp.task( 'build', function( callback ){
-    // ToDo: clean
-    runSequence( [ 'build:css', 'build:jsLibs', 'build:js' ], callback  );
-} );
+gulp.task('clean:css',
+    del.bind(null, [SETTINGS.CSS.DEST], {force: true})
+);
 
 
-gulp.task( 'browserSync', function(){
-    browserSync.init( {
+gulp.task('clean', ['clean:js', 'clean:css']);
+
+
+gulp.task('build', function(callback) {
+    return runSequence(
+        'clean',
+        ['build:css', 'build:jsLibs', 'build:js'],
+        callback
+    );
+});
+
+
+gulp.task('browserSync', function() {
+    browserSync.init({
         server: {
             baseDir: '../public/',
             https: true
         }
-    } );
+    });
 
-    return gulp.watch( '../public/**/*.*' ).on( 'change', browserSync.reload );
-} );
+    return gulp.watch('../public/**/*.*').on('change', browserSync.reload);
+});
 
-gulp.task( 'server', function( callback ){
-    runSequence( 'build', 'build:js:watch', 'build:css:watch', 'browserSync', callback  );
-} );
+gulp.task('server', function(callback) {
+    return runSequence(
+        'build',
+        ['build:js:watch', 'build:css:watch'],
+        'browserSync',
+        callback
+    );
+});
 
-gulp.task( 'gh-pages', function(){
-    return gulp.src( '../public/**/*' )
-        .pipe( ghPages() );
-} );
+gulp.task('gh-pages', function() {
+    return gulp.src('../public/**/*')
+        .pipe(ghPages());
+});
 
-gulp.task( 'default', [ 'server' ] );
+gulp.task('default', ['server']);
